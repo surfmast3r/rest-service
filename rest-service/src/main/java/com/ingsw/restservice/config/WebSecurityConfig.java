1eb0c50bb7b4aa5eb6548f8e47dabe0d3d6286ba
surfmast3r <surfmast3r@live.it>
2020-10-15 18:33:49 +0200

    changed accommodation_location endpoint security

--- rest-service/src/main/java/com/ingsw/restservice/config/WebSecurityConfig.java
+++ rest-service/src/main/java/com/ingsw/restservice/config/WebSecurityConfig.java
@@ -31,7 +31,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.csrf().disable()
 		.authorizeRequests().antMatchers("/","/review","/accommodation","/authenticate",
-				"/register","/review_view","/single_review_view","/accommodation_generic").permitAll()
+				"/register","/review_view","/single_review_view","/accommodation_generic","/accommodation_location").permitAll()
 		.antMatchers("/accommodation/**","/review/**").hasRole("ADMIN")
 		.anyRequest()
 		.authenticated()
